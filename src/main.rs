5c93e5ccc68a097e8996a455f1155455a3e8152a
Jake <Jake_Guy_11@protonmail.ch>
2021-06-23 11:52:58 -0400

    added parsing for basic params in instant mode

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,35 @@
+extern crate json;
 extern crate notify_rust;
 
 use notify_rust::Notification;
+
+// The user has selected instant mode
+fn queue_instant()
+{
+    let args: Vec<String> = std::env::args().skip(3).collect();
+
+    let mut notif = json::object!
+    {
+        title: "rremind",
+        body: "You did not set any body text for this reminder",
+        icon: "/home/jake/downloads/ogayu.jpg",
+        urgency: 1,
+        time: -1
+    };
+
+    for i in 0..args.len()
+    {
+        match args[i].as_str()
+        {
+            "-s" => { notif["title"] = json::JsonValue::String(args[i+1].to_string()); },
+            "-b" => { notif["body"] = json::JsonValue::String(args[i+1].to_string()); },
+            "-i" => { notif["icon"] = json::JsonValue::String(args[i+1].to_string()); },
+            "-u" => { notif["urgency"] = json::JsonValue::Number(args[i+1].parse::<i32>().unwrap_or(1).into());},
+            _ => {  }
+        }
+    }
+    println! ("{:?}", notif);
+}
 
 // This function will start the periodic loop that checks for notifications
 fn start_loop()
@@ -25,7 +54,7 @@ fn main()
     match mode.as_str()
     {
         "s" => { println! ("You've selected single mode"); },
-        "i" => { println! ("You've selected instant mode"); },
+        "i" => { queue_instant(); },
         "r" => { println! ("You've selected reccurant mode"); },
         _ => { println! ("Please enter a valid mode"); }
     }
