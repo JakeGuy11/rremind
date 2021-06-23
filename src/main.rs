80dc6fc0c014f8a9f952060e1705e5e0143b152e
Jake <Jake_Guy_11@protonmail.ch>
2021-06-23 11:06:42 -0400

    added mode argument handling

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,33 @@ extern crate notify_rust;
 
 use notify_rust::Notification;
 
+// This function will start the periodic loop that checks for notifications
+fn start_loop()
+{
+    println! ("Once start is implemented, it will be here");
+    std::process::exit(0);
+}
+
 fn main()
 {
-    Notification::new().summary("Title here").body("This is the body of the notification").icon("/home/jake/downloads/ogayu.jpg").show().expect("Some sort of error occurred!");
+    // Check if we want to start in add or start mode
+    let intent = std::env::args().nth(1).unwrap_or(String::from("start"));
+
+    // If the user wants to start, call start_loop
+    if &intent == "start" { start_loop(); }
+    else if &intent != "add" { eprintln! ("You must define a valid intent!"); std::process::exit(1); }
+
+    // Check what add mode they want to use
+    let mode = std::env::args().nth(2).unwrap_or(String::from("undefined"));
+
+    // Check what they want to do
+    match mode.as_str()
+    {
+        "s" => { println! ("You've selected single mode"); },
+        "i" => { println! ("You've selected instant mode"); },
+        "r" => { println! ("You've selected reccurant mode"); },
+        _ => { println! ("Please enter a valid mode"); }
+    }
+
+    // Notification::new().summary("Title here").body("This is the body of the notification").icon("/home/jake/downloads/ogayu.jpg").show().expect("Some sort of error occurred!");
 }
